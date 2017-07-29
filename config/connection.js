ade215190aa2644cfaca8a0b30e33b4de6cab5e2
LilTimmyBynum <timothy_ja_bynum@yahoo.com>
2017-07-29 04:44:15 -0700

    modified config for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 // connection.js
 
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
   password: "Diddybop!29!",
   database: "burgers_db"
 });
+};
 
 connection.connect(function(err) {
   if (err) {
